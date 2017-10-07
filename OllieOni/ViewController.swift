6ae6d3b1fe2fc22eea635af36090fe998adea62d
MusashiSakamoto <1292602b@gmail.com>
2017-10-07 16:04:26 +0900

    Initial Commit

---
+++ OllieOni/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OllieOni
+//
+//  Created by Musashi Sakamoto on 2017/10/07.
+//  Copyright © 2017年 Musashi Sakamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
