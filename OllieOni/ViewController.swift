56f064ac4d07954599fc2212d58781f52491c022
MusashiSakamoto <1292602b@gmail.com>
2017-10-31 22:55:35 +0900

    joystick added

--- OllieOni/ViewController.swift
+++ OllieOni/ViewController.swift
@@ -7,115 +7,34 @@
 //
 
 import UIKit
-import Speech
 
 class ViewController: UIViewController {
-  
-  private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ja-JP"))!
-  private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
-  @IBOutlet weak var movingStateLabel: UILabel!
-  private var recognitionTask: SFSpeechRecognitionTask?
-  private let audioEngine = AVAudioEngine()
-  @IBOutlet weak var recordButton: UIButton!
-  
+    
   @IBOutlet weak var stateLabel: UILabel!
   var robot: RKConvenienceRobot!
   var ledOn = false
-  var motionManager = CMMotionManager()
+    
+    lazy var joystick: JoyStickView = {
+        let joystick = JoyStickView(frame: CGRect(origin: self.view.center, size: CGSize(width: 100, height: 100)))
+        self.view.addSubview(joystick)
+        joystick.movable = false
+        joystick.alpha = 1.0
+        joystick.baseAlpha = 0.5
+        joystick.handleTintColor = UIColor.darkGray
+        return joystick
+    }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    recordButton.isEnabled = false
     // Do any additional setup after loading the view, typically from a nib.
     NotificationCenter.default.addObserver(self, selector: #selector(ViewController.appDidBecomeActive(_:)), name: .UIApplicationDidBecomeActive, object: nil)
     NotificationCenter.default.addObserver(self, selector: #selector(ViewController.appWillResignActive(_:)), name: .UIApplicationWillResignActive, object: nil)
     
     RKRobotDiscoveryAgent.shared().addNotificationObserver(self, selector: #selector(ViewController.handleRobotStateChangeNotification(_:)))
-  }
-  
-  private func startRecording() throws {
-    if let recognitionTask = recognitionTask {
-      recognitionTask.cancel()
-      self.recognitionTask = nil
-    }
-    
-    let audioSession = AVAudioSession.sharedInstance()
-    try audioSession.setCategory(AVAudioSessionCategoryRecord)
-    try audioSession.setMode(AVAudioSessionModeMeasurement)
-    try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
-    
-    recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
-    
-    guard let inputNode = audioEngine.inputNode else { return }
-    
-    guard let recognitionRequest = recognitionRequest else { return }
-    
-    recognitionRequest.shouldReportPartialResults = true
-    
-    recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest, resultHandler: {[weak self] (result, error) in
-      guard let `self` = self else { return }
-      var isFinal = false
-      print(result)
-      if let result = result {
-        let resultString = result.bestTranscription.formattedString
-        print("result: \(resultString)")
-        self.stateLabel.text = resultString
-        self.judgeResultAndDrive(resultString)
-        isFinal = result.isFinal
-      }
-      
-      if error != nil || isFinal {
-        self.audioEngine.stop()
-        inputNode.removeTap(onBus: 0)
-        
-        self.recognitionRequest = nil
-        self.recognitionTask = nil
-        self.recordButton.isEnabled = true
-      }
-    })
-    
-    let recordingFormat = inputNode.outputFormat(forBus: 0)
-    inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, time) in
-      self.recognitionRequest?.append(buffer)
-    }
-    
-    
-    audioEngine.prepare()
-    try audioEngine.start()
-  }
-  
-  func judgeResultAndDrive(_ string: String) {
-    switch string {
-    case "右":
-      robot.drive(withHeading: 90.0, andVelocity: 0.2)
-      break
-    case "左":
-      robot.drive(withHeading: 270.0, andVelocity: 0.2)
-      break
-    case "前":
-      robot.drive(withHeading: 0.0, andVelocity: 0.2)
-      break
-    case "後":
-      robot.drive(withHeading: 180.0, andVelocity: 0.2)
-      break
-    default:
-      print("コマンド失敗")
-    }
-    toggleLED()
-  }
-  
-  override func viewDidAppear(_ animated: Bool) {
-    speechRecognizer.delegate = self
-    
-    SFSpeechRecognizer.requestAuthorization { authStatus in
-      OperationQueue.main.addOperation {
-        switch authStatus {
-        case .authorized:
-          self.recordButton.isEnabled = true
-        default:
-          self.recordButton.isEnabled = false
-        }
-      }
+    joystick.monitor = { [weak self] angle, displacement in
+        let velocity = displacement * 0.2
+        guard let robot = self?.robot else { return }
+        robot.drive(withHeading: Float(angle), andVelocity: Float(velocity))
     }
   }
   
@@ -192,33 +111,6 @@ class ViewController: UIViewController {
       DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
         self.toggleLED()
       })
-    }
-  }
-  
-  @IBAction func recordButtonTapped(_ sender: UIButton) {
-    if audioEngine.isRunning {
-      print("stop")
-      movingStateLabel.text = "stopped"
-      audioEngine.stop()
-      recognitionRequest?.endAudio()
-      recordButton.isEnabled = false
-    } else {
-      robot.stop()
-      print("start")
-      movingStateLabel.text = "started"
-      toggleLED()
-      try! startRecording()
-    }
-  }
-  
-}
-
-extension ViewController: SFSpeechRecognizerDelegate {
-  func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
-    if available {
-      recordButton.isEnabled = true
-    } else {
-      recordButton.isEnabled = false
     }
   }
 }
